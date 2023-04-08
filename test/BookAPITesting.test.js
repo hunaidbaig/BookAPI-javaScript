f7f6d4438cd914b74deae3eee6a69451ba64de3d
AJkhan <atiajabbar39@gmail.com>
2023-04-08 17:47:19 +0500

    My 4th test case failed.

--- test/BookAPITesting.test.js
+++ test/BookAPITesting.test.js
@@ -52,3 +52,29 @@ test('I expect numberRead to return same when added same',()=>{
      // Assert
      expect(list).toBe(okayList);
 })
+
+//Given that I have a book in my list, when I call removeBook("<title>") with "title" representing the title of my book that I want to delete, then when I call getBooks() the book I deleted should no longer be there.
+
+test('I expect removeBook by title',()=>{
+    const title= "The Science";
+
+    bookObject1 = new Book('The Hobbit', 'J.R.R. Tolkein', 320, 1937);
+    bookObject2 = new Book('The History', 'J.R.R. Tolkein', 320, 1937);
+    bookObject3 = new Book('The Science', 'J.R.R. Tolkein', 320, 1937);
+    bookObject4 = new Book('The Math', 'J.R.R. Tolkein', 320, 1937);
+
+    readingObject = new ReadingList();
+
+    readingObject.addBook(bookObject1, 'January 1, 2020', 5);
+    readingObject.addBook(bookObject2, 'January 2, 2020', 4);
+    readingObject.addBook(bookObject3, 'January 3, 2020', 3);
+    readingObject.addBook(bookObject4, 'January 4, 2020', 4);
+
+    const totalBooks = readingObject.numberRead() - 1;
+    readingObject.removeBook(title);
+
+    const okayList = readingObject.numberRead();
+
+     // Assert
+     expect(totalBooks).toBe(okayList);
+})
