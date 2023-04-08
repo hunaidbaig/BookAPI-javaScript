95cb3bb450f62f40c276bb390564c59ce3f698ca
Hunaid Baig <hunaid.baig@xloopdigital.com>
2023-04-08 17:23:47 +0500

    My third test case failed

--- test/BookAPITesting.test.js
+++ test/BookAPITesting.test.js
@@ -30,3 +30,25 @@ test('I expect numberRead to return 1',()=>{
      // Assert
      expect(list).toBe(okayList);
 })
+
+//Given that I have an empty list, when I add additional books to the list I expect the numberRead to return the total number of books in my list.
+test('I expect numberRead to return same when added same',()=>{
+    const list = 3;
+
+    bookObject1 = new Book('The Hobbit', 'J.R.R. Tolkein', 320, 1937);
+    bookObject2 = new Book('The History', 'J.R.R. Tolkein', 320, 1937);
+    bookObject3 = new Book('The Science', 'J.R.R. Tolkein', 320, 1937);
+    bookObject4 = new Book('The Math', 'J.R.R. Tolkein', 320, 1937);
+
+    readingObject = new ReadingList();
+
+    readingObject.addBook(bookObject1, 'January 1, 2020', 5);
+    readingObject.addBook(bookObject2, 'January 2, 2020', 4);
+    readingObject.addBook(bookObject3, 'January 3, 2020', 3);
+    readingObject.addBook(bookObject4, 'January 4, 2020', 4);
+
+    const okayList = readingObject.numberRead();
+
+     // Assert
+     expect(list).toBe(okayList);
+})
